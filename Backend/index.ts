e3cfa6c39d514584af067272a8682544d6a22c08
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-30 12:01:25 -0300

    probando cosas

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 /**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
@@ -7,8 +9,18 @@
 const parser = new DOMParser();
 
 function initMap() {
+    let url='http://localhost:5000/api/restaurantes/';
+    axios
+        .get(url)
+        .then((result)=>{
 
-    alert("Entro")
+            console.log("h");
+             
+        })
+    
+    .catch((error)=>{
+        console.log(error);
+    });
   
   //Estas coordenadas son donde empieza el mapa
     const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
@@ -18,34 +30,19 @@ function initMap() {
         
     });
 
-    // Each PinView is paired with a MarkerView to demonstrate setting each parameter.
-
-    // Default marker with title text (no PinView).
-    /*
-    const markerViewWithText = new google.maps.marker.AdvancedMarkerView({
-        map,
-        position: { lat: 37.419, lng: -122.03 },
-        title: 'Title text for the marker at lat: 37.419, lng: -122.03',
-    });*/
-
-    // Adjust the scale.
-    const pinViewScaled = new google.maps.marker.PinView({
-        scale: 1.5,
-    });
-    /*
-    const markerViewScaled = new google.maps.marker.AdvancedMarkerView({
-        map,
-        position: { lat: 37.419, lng: -122.02 },
-        content: pinViewScaled.element,
-    });*/
-
-    // Change the background color.
+    //Cambios de diseño
     const pinViewStyled = new google.maps.marker.PinView({
         background: '#00B65C',
         borderColor: '#FFFFFF',
         glyphColor: 'white',
     });
 
+    //Array de ejemplo
+    let num = [7, 8, 9];
+    num.forEach(function (value) {
+    }); 
+
+    //Aplicamos los cambios
     const markerViewBackground = new google.maps.marker.AdvancedMarkerView({
         map,
         position: { lat: -34.61172463990566, lng: -58.422167399999985 },
@@ -84,6 +81,28 @@ function initMap() {
         position: { lat: 37.415, lng: -122.01 },
         content: pinViewNoGlyph.element,
     });*/
+
+        // Each PinView is paired with a MarkerView to demonstrate setting each parameter.
+
+    // Default marker with title text (no PinView).
+    /*
+    const markerViewWithText = new google.maps.marker.AdvancedMarkerView({
+        map,
+        position: { lat: 37.419, lng: -122.03 },
+        title: 'Title text for the marker at lat: 37.419, lng: -122.03',
+    });*/
+
+    // Adjust the scale.
+    const pinViewScaled = new google.maps.marker.PinView({
+        scale: 1.5,
+    });
+    /*
+    const markerViewScaled = new google.maps.marker.AdvancedMarkerView({
+        map,
+        position: { lat: 37.419, lng: -122.02 },
+        content: pinViewScaled.element,
+    });*/
+
 }
 
 declare global {
--- Backend/indexApiComida.js
+++ Backend/indexApiComida.js
@@ -1,5 +1,7 @@
 import getNutritionalInfo from './apiComida.js'
 
+console.log("llega")
+
 var url = 'https://world.openfoodfacts.org/api/v0/product/7790045824893.json';
 
 getNutritionalInfo(url)
