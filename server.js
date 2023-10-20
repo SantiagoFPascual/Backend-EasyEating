af62a111fe91b8b48f71a719ea42dace7b23f30e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-20 11:45:32 -0300

    tratando de arreglar postman

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import LimitacionesRouter from "./BaseDeDatos/src/controllers/limitacionesContro
 import UsuarioRouter from "./BaseDeDatos/src/controllers/usuariosController.js";
 import ProductoRouter from "./BaseDeDatos/src/controllers/productoController.js";
 import LimitacionXProductoRouter from "./BaseDeDatos/src/controllers/limitacionesXProductoController.js";
+import LimitacionXRestauranteRouter from "./BaseDeDatos/src/controllers/limitacionesXRestauranteController.js";
 
 const app  = express();
 const port = 5000;
@@ -16,12 +17,13 @@ app.use('/front', express.static('./BaseDeDatos/public'));
 
 //endpoint de los routers
 
-app.use("https://backendeasyeating.onrender.com/api/restaurantes", RestauranteRouter);
-app.use("https://backendeasyeating.onrender.com/api/limitaciones", LimitacionesRouter);
-app.use("https://backendeasyeating.onrender.com/api/usuarios", UsuarioRouter);
-app.use("https://backendeasyeating.onrender.com/api/productos", ProductoRouter);
-app.use("https://backendeasyeating.onrender.com/api/limitacionxproducto", LimitacionXProductoRouter);
+app.use("/api/restaurantes", RestauranteRouter);
+app.use("/api/limitaciones", LimitacionesRouter);
+app.use("/api/usuarios", UsuarioRouter);
+app.use("/api/productos", ProductoRouter);
+app.use("/api/limitacionxproducto", LimitacionXProductoRouter);
+app.use("/api/limitacionxrestaurante", LimitacionXRestauranteRouter);
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`"server" escuchando el en el puerto ${port} (http://localhost:${port}/front)`);
-});
+});*/
